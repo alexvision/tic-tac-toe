06bca5ad21c3731557acd382d96a81062da0d45a
Alex Manusu <alex.manusu@gmail.com>
2017-12-16 14:29:30 -0800

    Basic Tic Tac Toe Contract

--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,40 +1,100 @@
 pragma solidity 0.4.18;
 
-
 contract TicTacToe {
+    event NewGame(uint gameId, address player1, address player2);
 
-    // NOTE: All of the content here is just to provide some test examples
-
-    address owner;
-
-    // Storage variable must be public if we want automatically have a getter for it
-    uint256 public state;
-
-    event StateChanged(uint256 changedTo);
-    event Error(string msg);
-
-    function TicTacToe() {
-        owner = msg.sender;
+    struct Game {
+        address player1;
+        address player2;
+        address[3][3] board;
+        address winner;
+        bool hasWon;
+        address turn;
     }
-
-    // Function to test return value, state changes and event emission
-    function main(uint256 _newState) public returns(bool) {
-        if (msg.sender != owner) {
-            Error("You are not the owner");
-            return false;
+    
+    Game[] public games;
+    
+    function getBoard (uint id) public view returns (uint[3][3]) {
+        
+        uint[3][3] memory retBoard; 
+        address[3][3] memory board = games[id].board;
+        
+        for (uint i = 0; i < board.length; i++) {
+            for (uint j = 0; j < board.length; j++) {
+                if (board[i][j] == 0) {
+                    retBoard[i][j] = 0;
+                }
+                if (board[i][j] == games[id].player1) {
+                    retBoard[i][j] = 1;
+                }
+                if (board[i][j] == games[id].player2) {
+                    retBoard[i][j] = 2;
+                }
+            }
         }
-        state = _newState;
-        StateChanged(_newState);
-        return true;
+        return retBoard;
     }
-
-    // Function to test VM exception
-    function other(uint256 _newState) public returns(bool) {
-        state = _newState;
-        StateChanged(_newState);
-        // Require goes after state changes in order to demonstrate changes reversion
-        require(msg.sender != owner);
-        return true;
+    
+    function createGame(address _player1, address _player2) public returns (uint) {
+        address[3][3] memory board;
+        uint id = games.push(Game(_player1, _player2, board, 0, false, _player1));
+        NewGame(id, _player1, _player2);
+        return id - 1;
     }
+    
+    function checkWin(Game _game) private pure returns (bool) {
+        address[3][3] memory board = _game.board;
+        for (uint i = 0; i < board.length; i++) {
+            address firstVal = board[i][0]; 
+            //  check horizontal;
+            for (uint j = 0; j < board.length; j++) {
+                if (firstVal != board[i][j]) {
+                    break;
+                }
+                if (j == 2) {
+                    _game.winner = firstVal;
+                    _game.hasWon = true;
+                    return true;
+                }
+            }
+            // check vertical
+            for (uint k = 0; k < board.length; k++) {
+                if (firstVal != board[k][i]) {
+                    break;
+                }
+                if (k == 2) {
+                    _game.winner = firstVal;
+                    _game.hasWon = true;
+                    return true;
+                }
+            }
+        }
+        // check diagonals;
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            _game.winner = board[0][0];
+            _game.hasWon = true;
+            return true;
+        }
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            _game.winner = _game.board[0][0];
+            _game.hasWon = true;
+            return true;
+        }
+        return false;
+    }
+    
+    function makeMove(uint _game, address _player, uint8 _x, uint8 _y) public returns (address) {
+        Game memory g = games[_game];
+        require(g.turn == _player);
+        require(g.board[_x][_y] == 0);
+        require(g.hasWon != true);
+        games[_game].board[_x][_y] = _player;
+        games[_game].turn = _player == g.player1 ? g.player2 : g.player1;
+        checkWin(g);
+        if (g.hasWon == false) {
+            return 0;
+        }
+        return _player;
+    }    
 
 }
